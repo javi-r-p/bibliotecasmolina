3a72d46c7ff6f5b3e1c5916cf261bb05c61885ed
javi-r-p <7460103@alu.murciaeduca.es>
2025-03-21 11:52:59 +0100

    Crear archivo index.php y actualizar el archivo de la configuración del VirtualHost de Apache2

---
+++ index.php
@@ -0,0 +1,10 @@
+<html>
+	<head>
+		<title>Bibliotecas de Molina de Segura</title>
+		<meta charset="UTF-8">
+	</head>
+	<body>
+		<h1 style="text-align: center;">¡Bienvenido!</h1>
+		<hr>
+	</body>
+</html>
